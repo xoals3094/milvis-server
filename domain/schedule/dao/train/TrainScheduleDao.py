852657f39e25913b5d7a710077ddb7403a38dec8
taemin <xoals3094@naver.com>
2024-02-29 14:46:15 +0900

    refactor: api 요청 관련 코드 분리

--- domain/schedule/dao/train/TrainScheduleDao.py
+++ domain/schedule/dao/train/TrainScheduleDao.py
@@ -4,18 +4,12 @@ from typing import List
 from datetime import datetime
 from functools import partial
 from abc import *
-from config.api import SERVICE_KEY
+from config.api import SERVICE_KEY, URL
+from etc.train_info import trains
 
 import xml.etree.ElementTree as ET
 import requests
 import asyncio
-
-
-trains = [{'id': '00', 'name': 'KTX'}, {'id': '01', 'name': '새마을호'}, {'id': '02', 'name': '무궁화호'},
-          {'id': '03', 'name': '통근열차'}, {'id': '04', 'name': '누리로'}, {'id': '06', 'name': 'AREX직통'},
-          {'id': '07', 'name': 'KTX-산천(A-type)'}, {'id': '08', 'name': 'ITX-새마을'}, {'id': '09', 'name': 'ITX-청춘'},
-          {'id': '10', 'name': 'KTX-산천(B-type)'}, {'id': '16', 'name': 'KTX-이음'}, {'id': '17', 'name': 'SRT'}]
-URL = 'http://apis.data.go.kr/1613000/TrainInfoService/getStrtpntAlocFndTrainInfo?serviceKey={SERVICE_KEY}&pageNo=1&numOfRows=100&_type=xml&depPlaceId={depart_station_code}&arrPlaceId={arrive_station_code}&depPlandTime={depart_datetime}&trainGradeCode={train_code}'
 
 
 class TrainScheduleDao(metaclass=ABCMeta):
