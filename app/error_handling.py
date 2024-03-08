18ab9c8fcf577eaa257a0ae5058e4655f6c39465
TaeMin <xoals3094@naver.com>
2024-03-08 12:50:54 +0900

    style: 에러 핸들러 리턴 포멧 변경

--- app/error_handling.py
+++ app/error_handling.py
@@ -6,4 +6,4 @@ from exceptions import PersistenceException
 def error_handling(app: FastAPI):
     @app.exception_handler(PersistenceException.ResourceNotFoundException)
     def resource_not_found_exception(reqeust, exc: PersistenceException.ResourceNotFoundException):
-        return JSONResponse(content=exc.msg, status_code=status.HTTP_404_NOT_FOUND)
+        return JSONResponse(content={'msg': '일치하는 데이터를 찾을 수 없습니다'}, status_code=status.HTTP_404_NOT_FOUND)
